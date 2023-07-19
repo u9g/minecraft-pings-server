f9e95ab4f2ae6f1dc242dcef80224c27894a9291
U9G <git@u9g.dev>
2023-07-19 15:22:48 -0400

    format and add logging

--- index.ts
+++ index.ts
@@ -1,43 +1,50 @@
+import "fs";
+import { appendFile } from "fs/promises";
+
 Bun.serve<{ username: string; ip: string }>({
-	websocket: {
-		message(ws, message) {
-			if (typeof message !== "string") return
-			try {
-				const packet = JSON.parse(message)
-				console.log(`received packet: ${JSON.stringify(packet)}`)
-				if (packet?.ip?.includes('midnightsky.') === true)
-						packet.ip = 'midnightsky'
+  websocket: {
+    message(ws, message) {
+      if (typeof message !== "string") return;
+      try {
+        const packet = JSON.parse(message);
+        console.log(`received packet: ${JSON.stringify(packet)}`);
+        if (packet?.ip?.includes("midnightsky.") === true)
+          packet.ip = "midnightsky";
 
-				if (packet.type === "connected") {					
-					ws.data = { ip: packet.ip, username: packet.username }
+        if (packet.type === "connected") {
+          ws.data = { ip: packet.ip, username: packet.username };
+          appendFile(
+            "./log.txt",
+            JSON.stringify({ ip: packet.ip, username: packet.username })
+          );
 
-					ws.subscribe(packet.ip)
-				} else if (packet.type === "disconnected") {
-					ws.unsubscribe(packet.ip)
-					ws.data.ip = ""
-				} else if (packet.type === "ping") {
-					if (ws.data.ip === "") return
-					console.log("sending ping: " + JSON.stringify(ws.data))
-					ws.publish(
-						ws.data.ip,
-						JSON.stringify({
-							x: packet.x,
-							y: packet.y,
-							z: packet.z,
-							username: ws.data.username,
-						})
-					)
-				}
-			} catch (e) {
-				console.log(e)
-				return
-			}
-		},
-	},
-	fetch(request, server) {
-		const upgraded = server.upgrade(request)
-		if (!upgraded) {
-			return new Response("Upgrade failed", { status: 400 })
-		}
-	},
-})
+          ws.subscribe(packet.ip);
+        } else if (packet.type === "disconnected") {
+          ws.unsubscribe(packet.ip);
+          ws.data.ip = "";
+        } else if (packet.type === "ping") {
+          if (ws.data.ip === "") return;
+          console.log("sending ping: " + JSON.stringify(ws.data));
+          ws.publish(
+            ws.data.ip,
+            JSON.stringify({
+              x: packet.x,
+              y: packet.y,
+              z: packet.z,
+              username: ws.data.username,
+            })
+          );
+        }
+      } catch (e) {
+        console.log(e);
+        return;
+      }
+    },
+  },
+  fetch(request, server) {
+    const upgraded = server.upgrade(request);
+    if (!upgraded) {
+      return new Response("Upgrade failed", { status: 400 });
+    }
+  },
+});
